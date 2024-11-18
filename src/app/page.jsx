73759824589397486bc1f87dc9c57446eb7c2e97
z3a1 <paul.colonia@outlook.com>
2024-11-18 14:39:53 -0800

    Init commit

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1>JetScoot</h1>
+      <div id = "landingHeader">
+
+      </div>
+      <div id = "desc">
+        
+      </div>
+    </div>
+  );
+}
